f4602989fe3c31282879e27cb3885b0e97204f1b
alixdebeusscher <42241229+alixdebeusscher@users.noreply.github.com>
2018-12-30 08:58:21 +0100

    tt

--- graph_with_networkx_alix.py
+++ graph_with_networkx_alix.py
@@ -21,7 +21,7 @@ def my_circular_layout(Graph):
                 
         return my_circ_layout
 
-def my_graph_generator(file):
+def my_graph_generator(file, value):
     G = nx.Graph()
     source = []
     node_indices = []
@@ -42,100 +42,14 @@ def my_graph_generator(file):
         size_array.append(G.node[nd]['size'])
     
     
-
-<<<<<<< HEAD
-    plt.savefig("my_graph.png")     
-
+    
+    pos =  []
+    if value =='circ':
+        pos = my_circular_layout(G)
+    if value =='oth':
+        pos = nx.fruchterman_reingold_layout(G)
     #nx.draw(G,pos = my_circular_layout(G),with_labels=True,nodecolor = 'b',edge_color = 'r',node_size=size_array,node_shape='o')
-    return [G,my_circular_layout(G)]
+    return [G,pos]
 
-graph = my_graph_generator('my_data_set.txt')
-=======
-    plt.savefig("my_graph.png")
-    
-    
-    def my_circular_layout(Graph):
-        Nodes = G.nodes
-        N = len(Nodes)
-        N_list = np.arange(0,N)
-        circ = [i*2*math.pi/N for i in N_list]
-        x = [math.cos(j) for j in circ]
-        y = [math.sin(j) for j in circ]
-        my_circ_layout = {}
-        k = 0
-        for nd in Nodes:
-                my_circ_layout[nd] = [x[k],y[k]]
-                Nodes[nd]['pos'] = [x[k],y[k]]
-                k+=1
-                
-        return my_circ_layout
-        
-
-    
-
-    nx.draw(G,pos = my_circular_layout(G),with_labels=True,nodecolor = 'b',edge_color = 'r',node_size=size_array,node_shape='o')
-
-
-
-    edge_trace = go.Scatter(
-    x=[],
-    y=[],
-    line=dict(width=1,color='#f00'),
-    hoverinfo='none',
-    mode='lines')
-
-    for edge in G.edges():
-        x0, y0 = G.node[edge[0]]['pos']
-        x1, y1 = G.node[edge[1]]['pos']
-        edge_trace['x'] += tuple([x0, x1, None])
-        edge_trace['y'] += tuple([y0, y1, None])
-
-    node_trace = go.Scatter(
-    x=[],
-    y=[],
-    text=[],
-    mode='markers',
-    hoverinfo='text',
-    marker=dict(
-        showscale=True,
-        # colorscale options
-        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-        colorscale='YlGnBu',
-        reversescale=True,
-        color=[],
-        size=size_array,
-        colorbar=dict(
-            thickness=15,
-            title='Node Connections',
-            xanchor='left',
-            titleside='right'
-        ),
-        line=dict(width=2)))
-
-    for node in G.nodes():
-        x, y = G.node[node]['pos']
-        node_trace['x'] += tuple([x])
-        node_trace['y'] += tuple([y])
-
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_trace['marker']['color']+=tuple([len(adjacencies[1])])
-        node_info = '# of connections: '+str(len(adjacencies[1]))
-        node_trace['text']+=tuple([node_info])
-        
-    fig = go.Figure(data=[edge_trace, node_trace],
-                    layout=go.Layout(
-                            title='My Network',
-                            titlefont=dict(size=16),
-                            showlegend=False,
-                            hovermode='closest',
-                            margin=dict(b=20,l=5,r=5,t=40),
-                           ))
-    
-    ply.tools.set_credentials_file(username='delhayead', api_key='Twixfb2Wjh6VdXxB2dvT')
-    py.iplot(fig, filename='my_networkx')
-    return [G,my_circular_layout]
-    
-    
->>>>>>> 01bd083aa7033da70c18e1e2f43166e401dcb6e3
+def get_graph(value):
+    return my_graph_generator('my_data_set.txt', value)
--- test2.py
+++ test2.py
@@ -4,38 +4,35 @@ import plotly.offline as offline
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import graph_with_networkx as gw
+import graph_with_networkx_alix as gw
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 from plotly.tools import FigureFactory as FF
 import networkx as nx
 from plotly.offline import download_plotlyjs, init_notebook_mode,  iplot, plot
 
-def make_annotations(pos, anno_text, font_size=14, font_color='rgb(10,10,10)'):
-    pos=list(pos.items())
-    L=len(pos)
-    print(pos)
-    if len(anno_text)!=L:
+def make_annotations(labels, Xn, Yn, font_size=14, font_color='rgb(10,10,10)'):
+    L=len(Xn)
+    if len(labels)!=L:
         raise ValueError('The lists pos and text must have the same len')
     annotations = []
     for k in range(L):
-        annotations.append(dict(text=anno_text[k], 
-                                x=pos[k][1][0], 
-                                y=pos[k][1][1]+0.075,#this additional value is chosen by trial and error
+        annotations.append(dict(text=labels[k], 
+                                x=Xn[k]+0.075, 
+                                y=Yn[k]+0.075,
                                 xref='x1', yref='y1',
                                 font=dict(color= font_color, size=font_size),
                                 showarrow=False)
                           )
     return annotations
- 
 
-def get_data():
-    tab=gw.graph
+def get_data(lab, value):
+    tab=gw.get_graph(value)
     G=tab[0]
     pos=tab[1] 
     labels = [ k for k,v in pos.items()]
-    Xn=[]
-    Yn=[]
+    Xn = []
+    Yn = []
     for key in pos.keys():
         x = (pos.get(key))[0]
         y = (pos.get(key))[1]
@@ -87,9 +84,11 @@ def get_data():
        
     ),
     hovermode='closest',
-    annotations=make_annotations(pos, labels),
+    #annotations=make_annotations(),
     plot_bgcolor='#efecea', #set background color            
     )
+    if lab==1:
+        layout.update(annotations=make_annotations(labels, Xn, Yn))
     return [trace_edges, trace_nodes],layout
     
 
@@ -98,34 +97,49 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-allo=get_data()          
-data=allo[0]
-layout=allo[1]
+#allo=get_data(lab=1)          
+#data=allo[0]
+#layout=allo[1]
 app.layout = html.Div(children=[
     html.H4(children='Hello Dash', style={
                     'textAlign': 'center'
                 }),
         
+            dcc.Checklist(
+                id='list',
+                options=[{'label': 'Labels', 'value': 'lab'}],
+                values=['lab']
+            ),
+            dcc.Dropdown(id='algo', options=[
+                    {'label': 'Circular', 'value': 'circ'},
+                    {'label': 'Other', 'value': 'oth'}
+                    ],
+                    value='circ'
             
-            dcc.Input(id='my-id', value='Yes', type='text'),
-            html.Div(id='my-div'),
-            
+            ),
             dcc.Graph(
-                id='example-graph',
-                figure={
-                    'data': data,
-                    'layout': layout
-                }, style={'display': 'inline-block'}
+                id='graphic',
+#                figure={
+#                    'data': data,
+#                    'layout': layout
+#                }, 
+                style={'display': 'inline-block'}
             )
     ])
     
 @app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
+    Output(component_id='graphic', component_property='figure'),
+    [Input(component_id='list', component_property='values'),
+     Input(component_id='algo', component_property='value')]
 )
-def update_output_div(input_value):
-    
-    return 'You\'ve entered "{}"'.format(input_value)
+def update_graph(list_l, algo_l):
+    if 'lab' in list_l:
+        allo=get_data(1, algo_l)  
+    else:
+        allo=get_data(0, algo_l)       
+    data=allo[0]
+    layout=allo[1]
+    return  { 'data': data,'layout': layout}
     
 
 if __name__ == '__main__':
